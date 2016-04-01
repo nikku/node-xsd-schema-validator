d6110a08daac2b461d7df59a3e1bfedc017be198
Nico Rehwaldt <git_nikku@nixis.de>
2016-04-01 13:52:58 +0200

    test(project): verify UTF-8 document handling

--- test/spec/validatorSpec.js
+++ test/spec/validatorSpec.js
@@ -21,6 +21,24 @@ describe('validator', function() {
     });
   });
 
+
+  it('should validate correct xml with international characters', function(done) {
+
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+              '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" name="Obračun potnih stroškov" targetNamespace="http://activiti.org/bpmn">' +
+              '</bpmn2:definitions>';
+
+    validator.validateXML(xml, SAMPLE_SCHEMA, function(err, result) {
+      if (err) {
+        done(err);
+      } else {
+        expect(result.valid).toBe(true);
+        done();
+      }
+    });
+  });
+
+
   it('should validate incorrect xml', function(done) {
 
     var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
@@ -32,4 +50,5 @@ describe('validator', function() {
       done();
     });
   });
+
 });
