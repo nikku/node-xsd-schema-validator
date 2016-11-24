890308564e45feef7c8c84330d1bc37bbfd2c2c5
Nico Rehwaldt <git_nikku@nixis.de>
2016-11-24 21:48:07 +0100

    test(validator): restructure + add stream tests

---
+++ test/validator.js
@@ -0,0 +1,109 @@
+var validator = require('../lib/validator');
+
+var fs = require('fs');
+
+var BPMN_SCHEMA = 'test/xsd/BPMN20.xsd';
+var UMLAUT_SCHEMA = 'test/xsd/UmlautSchema.xsd';
+
+var BPMN_FILE = 'test/diagram.bpmn';
+var INVALID_BPMN_FILE = 'test/invalid.bpmn';
+
+
+describe('validator', function() {
+
+  describe('should validate xml string', function() {
+
+    it('correct', function(done) {
+
+      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+                '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
+                '</bpmn2:definitions>';
+
+      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
+        if (err) {
+          done(err);
+        } else {
+          expect(result.valid).toBe(true);
+          done();
+        }
+      });
+    });
+
+
+    it('broken', function(done) {
+
+      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+                '<bpmn2:definitions unknownAttr="BOOO" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
+                '</bpmn2:definitions>';
+
+      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
+        expect(err).toBeDefined();
+        done();
+      });
+    });
+
+
+    it('with international characters', function(done) {
+
+      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+                '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" name="Obračun potnih stroškov" targetNamespace="http://activiti.org/bpmn">' +
+                '</bpmn2:definitions>';
+
+      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
+        if (err) {
+          done(err);
+        } else {
+          expect(result.valid).toBe(true);
+          done();
+        }
+      });
+    });
+
+
+    it('with umlaut', function(done) {
+
+      var xml = (
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<Test>ü</Test>'
+      );
+
+      validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
+        expect(result.valid).toBe(true);
+        done();
+      });
+    });
+
+  });
+
+
+  describe('should validate XML stream', function() {
+
+    it('correct', function(done) {
+
+      var xmlStream = fs.createReadStream(BPMN_FILE, { encoding: 'UTF-8' });
+
+      validator.validateXML(xmlStream, BPMN_SCHEMA, function(err, result) {
+
+        if (err) {
+          done(err);
+        } else {
+          expect(result.valid).toBe(true);
+          done();
+        }
+      });
+    });
+
+
+    it('broken', function(done) {
+
+      var xmlStream = fs.createReadStream(INVALID_BPMN_FILE, { encoding: 'UTF-8' });
+
+      validator.validateXML(xmlStream, BPMN_SCHEMA, function(err, result) {
+        expect(err).toBeDefined();
+        done();
+      });
+    });
+
+  });
+
+});
