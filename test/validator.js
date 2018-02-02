5dda5d98f2a84f454306394774301bba637dd7d4
Nico Rehwaldt <git_nikku@nixis.de>
2018-02-02 09:36:42 +0100

    chore(tests): move / rename schema resources

--- test/validator.js
+++ test/validator.js
@@ -2,8 +2,8 @@ var validator = require('../lib/validator');
 
 var fs = require('fs');
 
-var BPMN_SCHEMA = 'test/xsd/BPMN20.xsd';
-var UMLAUT_SCHEMA = 'test/xsd/UmlautSchema.xsd';
+var BPMN_SCHEMA = 'test/xsd/bpmn/BPMN20.xsd';
+var UMLAUT_SCHEMA = 'test/xsd/Umlauts.xsd';
 
 var BPMN_FILE = 'test/diagram.bpmn';
 var INVALID_BPMN_FILE = 'test/invalid.bpmn';
@@ -13,7 +13,7 @@ describe('validator', function() {
 
   describe('should validate xml string', function() {
 
-    it('correct', function(done) {
+    it('valid', function(done) {
 
       var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
@@ -30,7 +30,7 @@ describe('validator', function() {
     });
 
 
-    it('broken', function(done) {
+    it('invalid', function(done) {
 
       var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions unknownAttr="BOOO" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
@@ -67,17 +67,33 @@ describe('validator', function() {
     });
 
 
-    it('with umlaut', function(done) {
+    describe('with umlaut', function() {
 
-      var xml = (
-        '<?xml version="1.0" encoding="UTF-8"?>' +
-        '<Test>ü</Test>'
-      );
+      it('valid', function(done) {
+        var xml = (
+          '<?xml version="1.0" encoding="UTF-8"?>' +
+          '<Test>ü</Test>'
+        );
 
-      validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
-        expect(result.valid).toBe(true);
-        done();
+        validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
+          expect(result.valid).toBe(true);
+          done();
+        });
       });
+
+
+      it('invalid', function(done) {
+        var xml = (
+          '<?xml version="1.0" encoding="UTF-8"?>' +
+          '<AÖ></AÖ>'
+        );
+
+        validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
+          expect(result.valid).toBe(false);
+          done();
+        });
+      });
+
     });
 
   });
@@ -85,7 +101,7 @@ describe('validator', function() {
 
   describe('should validate XML stream', function() {
 
-    it('correct', function(done) {
+    it('valid', function(done) {
 
       var xmlStream = fs.createReadStream(BPMN_FILE, { encoding: 'UTF-8' });
 
@@ -101,7 +117,7 @@ describe('validator', function() {
     });
 
 
-    it('broken', function(done) {
+    it('invalid', function(done) {
 
       var xmlStream = fs.createReadStream(INVALID_BPMN_FILE, { encoding: 'UTF-8' });
 
@@ -116,7 +132,7 @@ describe('validator', function() {
 
   describe('should validate Buffer', function() {
 
-    it('correct', function(done) {
+    it('valid', function(done) {
 
       var buffer = new Buffer('<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
@@ -134,7 +150,7 @@ describe('validator', function() {
     });
 
 
-    it('broken', function(done) {
+    it('invalid', function(done) {
 
       var buffer = new Buffer('<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions unknownAttr="BOOO" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
@@ -151,33 +167,38 @@ describe('validator', function() {
 
   describe('should validate { file }', function() {
 
-    it('correct', function(done) {
+    describe('bpmn', function() {
 
-      validator.validateXML({ file: BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
+      it('valid', function(done) {
 
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).toBe(true);
+        validator.validateXML({ file: BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
+
+          if (err) {
+            done(err);
+          } else {
+            expect(result.valid).toBe(true);
+            done();
+          }
+        });
+      });
+
+
+      it('invalid', function(done) {
+
+        validator.validateXML({ file: INVALID_BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
+          expect(err).toBeTruthy();
           done();
-        }
+        });
       });
-    });
 
-
-    it('broken', function(done) {
-
-      validator.validateXML({ file: INVALID_BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
-        expect(err).toBeTruthy();
-        done();
-      });
     });
 
   });
 
+
   describe('should not validate unsupported type for xml', function() {
 
-    it('correct', function(done) {
+    it('valid', function(done) {
 
       validator.validateXML(['this is not valid'], BPMN_SCHEMA, function(err, result) {
         expect(err).toBeTruthy();
