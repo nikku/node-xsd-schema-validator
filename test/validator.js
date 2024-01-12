65f1e82515acf766bbe0a58627de07e8c3f6af6d
Nico Rehwaldt <git_nikku@nixis.de>
2024-01-12 23:37:05 +0100

    chore: migrate to promise-based API
    Closes https://github.com/nikku/node-xsd-schema-validator/issues/32

--- test/validator.js
+++ test/validator.js
@@ -1,17 +1,17 @@
-var validator = require('..');
+const validator = require('..');
 
-var expect = require('chai').expect;
+const { expect } = require('chai');
 
-var fs = require('fs');
+const fs = require('fs');
 
-var BPMN_SCHEMA = 'test/xsd/bpmn/BPMN20.xsd';
-var UMLAUT_SCHEMA = 'test/xsd/Umlauts.xsd';
-var INCLUDE_SCHEMA = 'test/xsd/Include.xsd';
-var IMPORT_SCHEMA = 'test/xsd/Import.xsd';
-var OTHER_SCHEMA = 'test/xsd/Other.xsd';
+const BPMN_SCHEMA = 'test/xsd/bpmn/BPMN20.xsd';
+const UMLAUT_SCHEMA = 'test/xsd/Umlauts.xsd';
+const INCLUDE_SCHEMA = 'test/xsd/Include.xsd';
+const IMPORT_SCHEMA = 'test/xsd/Import.xsd';
+const OTHER_SCHEMA = 'test/xsd/Other.xsd';
 
-var BPMN_FILE = 'test/diagram.bpmn';
-var INVALID_BPMN_FILE = 'test/invalid.bpmn';
+const BPMN_FILE = 'test/diagram.bpmn';
+const INVALID_BPMN_FILE = 'test/invalid.bpmn';
 
 
 describe('validator', function() {
@@ -24,26 +24,23 @@ describe('validator', function() {
     beforeEach(cleanup);
 
 
-    it('should pre-compile', function(done) {
-      validator.setup(done);
+    it('should pre-compile', function() {
+      return validator.setup();
     });
 
 
-    it('should compile on the fly (without prior setup)', function(done) {
+    it('should compile on the fly (without prior setup)', async function() {
 
       // given
-      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      const xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
                 '</bpmn2:definitions>';
 
-      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).to.be.true;
-          done();
-        }
-      });
+      // when
+      const { valid } = await validator.validateXML(xml, BPMN_SCHEMA);
+
+      // then
+      expect(valid).to.be.true;
     });
 
   });
@@ -51,86 +48,101 @@ describe('validator', function() {
 
   describe('should validate xml string', function() {
 
-    it('valid', function(done) {
+    it('valid', async function() {
 
       // given
-      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      const xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
                 '</bpmn2:definitions>';
 
-      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).to.be.true;
-          done();
-        }
-      });
+      // when
+      const { valid } = await validator.validateXML(xml, BPMN_SCHEMA);
+
+      // then
+      expect(valid).to.be.true;
     });
 
 
-    it('invalid', function(done) {
+    it('invalid', async function() {
 
-      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      // given
+      const xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions unknownAttr="BOOO" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
                 '</bpmn2:definitions>';
 
-      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
-        expect(err).to.exist;
+      let err;
 
-        // correct error message
-        expect(err.message).match(/Attribute 'unknownAttr' is not allowed to appear in element 'bpmn2:definitions'/);
+      // when
+      try {
+        await validator.validateXML(xml, BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
 
-        // and line number
-        expect(err.message).match(/\(1:476\)/);
+      // then
+      expect(err).to.exist;
 
-        done();
-      });
+      // correct error message
+      expect(err.message).match(/Attribute 'unknownAttr' is not allowed to appear in element 'bpmn2:definitions'/);
+
+      // and line number
+      expect(err.message).match(/\(1:476\)/);
     });
 
 
-    it('with international characters', function(done) {
+    it('with international characters', async function() {
 
-      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      // given
+      const xml = '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" name="Obračun potnih stroškov" targetNamespace="http://activiti.org/bpmn">' +
                 '</bpmn2:definitions>';
 
-      validator.validateXML(xml, BPMN_SCHEMA, function(err, result) {
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).to.be.true;
-          done();
-        }
-      });
+      // when
+      const { valid } = await validator.validateXML(xml, BPMN_SCHEMA);
+
+      // then
+      expect(valid).to.be.true;
     });
 
 
     describe('with umlaut', function() {
 
-      it('valid', function(done) {
-        var xml = (
+      it('valid', async function() {
+
+        // given
+        const xml = (
           '<?xml version="1.0" encoding="UTF-8"?>' +
           '<Test xmlns="http://umlauts">ü</Test>'
         );
 
-        validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
-          expect(result.valid).to.be.true;
-          done();
-        });
+        // when
+        const { valid } = await validator.validateXML(xml, UMLAUT_SCHEMA);
+
+        // then
+        expect(valid).to.be.true;
       });
 
 
-      it('invalid', function(done) {
-        var xml = (
+      it('invalid', async function() {
+
+        // given
+        const xml = (
           '<?xml version="1.0" encoding="UTF-8"?>' +
           '<AÖ xmlns="http://umlauts"></AÖ>'
         );
 
-        validator.validateXML(xml, UMLAUT_SCHEMA, function(err, result) {
-          expect(result.valid).to.be.false;
-          done();
-        });
+        let err;
+
+        // when
+        try {
+          await validator.validateXML(xml, UMLAUT_SCHEMA);
+        } catch (_err) {
+          err = _err;
+        }
+
+        // then
+        expect(err).to.exist;
+        expect(err.valid).to.be.false;
       });
 
     });
@@ -140,30 +152,35 @@ describe('validator', function() {
 
   describe('should validate XML stream', function() {
 
-    it('valid', function(done) {
+    it('valid', async function() {
 
-      var xmlStream = fs.createReadStream(BPMN_FILE, { encoding: 'UTF-8' });
+      // given
+      const xmlStream = fs.createReadStream(BPMN_FILE, { encoding: 'UTF-8' });
 
-      validator.validateXML(xmlStream, BPMN_SCHEMA, function(err, result) {
+      // when
+      const { valid } = await validator.validateXML(xmlStream, BPMN_SCHEMA);
 
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).to.be.true;
-          done();
-        }
-      });
+      // then
+      expect(valid).to.be.true;
     });
 
 
-    it('invalid', function(done) {
+    it('invalid', async function() {
 
-      var xmlStream = fs.createReadStream(INVALID_BPMN_FILE, { encoding: 'UTF-8' });
+      // given
+      const xmlStream = fs.createReadStream(INVALID_BPMN_FILE, { encoding: 'UTF-8' });
 
-      validator.validateXML(xmlStream, BPMN_SCHEMA, function(err, result) {
-        expect(err).to.exist;
-        done();
-      });
+      let err;
+
+      // when
+      try {
+        await validator.validateXML(xmlStream, BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
+
+      // then
+      expect(err).to.exist;
     });
 
   });
@@ -171,38 +188,43 @@ describe('validator', function() {
 
   describe('should validate Buffer', function() {
 
-    it('valid', function(done) {
+    it('valid', async function() {
 
-      var buffer = createBuffer(
+      // given
+      const buffer = createBuffer(
         '<?xml version="1.0" encoding="UTF-8"?>' +
         '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
         '</bpmn2:definitions>'
       );
 
-      validator.validateXML(buffer, BPMN_SCHEMA, function(err, result) {
+      // when
+      const { valid } = await validator.validateXML(buffer, BPMN_SCHEMA);
 
-        if (err) {
-          done(err);
-        } else {
-          expect(result.valid).to.be.true;
-          done();
-        }
-      });
+      // then
+      expect(valid).to.be.true;
     });
 
 
-    it('invalid', function(done) {
+    it('invalid', async function() {
 
-      var buffer = createBuffer(
+      // given
+      const buffer = createBuffer(
         '<?xml version="1.0" encoding="UTF-8"?>' +
         '<bpmn2:definitions unknownAttr="BOOO" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
         '</bpmn2:definitions>'
       );
 
-      validator.validateXML(buffer, BPMN_SCHEMA, function(err, result) {
-        expect(err).to.exist;
-        done();
-      });
+      let err;
+
+      // when
+      try {
+        await validator.validateXML(buffer, BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
+
+      // then
+      expect(err).to.exist;
     });
 
   });
@@ -212,26 +234,30 @@ describe('validator', function() {
 
     describe('bpmn', function() {
 
-      it('valid', function(done) {
+      it('valid', async function() {
 
-        validator.validateXML({ file: BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
+        // when
+        const { valid } = await validator.validateXML({ file: BPMN_FILE }, BPMN_SCHEMA);
 
-          if (err) {
-            done(err);
-          } else {
-            expect(result.valid).to.be.true;
-            done();
-          }
-        });
+        // then
+        expect(valid).to.be.true;
       });
 
 
-      it('invalid', function(done) {
+      it('invalid', async function() {
 
-        validator.validateXML({ file: INVALID_BPMN_FILE }, BPMN_SCHEMA, function(err, result) {
-          expect(err).to.exist;
-          done();
-        });
+        // given
+        let err;
+
+        // when
+        try {
+          await validator.validateXML({ file: INVALID_BPMN_FILE }, BPMN_SCHEMA);
+        } catch (_err) {
+          err = _err;
+        }
+
+        // then
+        expect(err).to.exist;
       });
 
     });
@@ -239,26 +265,30 @@ describe('validator', function() {
 
     describe('with include', function() {
 
-      it('valid', function(done) {
+      it('valid', async function() {
 
-        validator.validateXML({ file: 'test/umlauts.xml' }, INCLUDE_SCHEMA, function(err, result) {
+        // when
+        const { valid } = await validator.validateXML({ file: 'test/umlauts.xml' }, INCLUDE_SCHEMA);
 
-          if (err) {
-            done(err);
-          } else {
-            expect(result.valid).to.be.true;
-            done();
-          }
-        });
+        // then
+        expect(valid).to.be.true;
       });
 
 
-      it('invalid', function(done) {
+      it('invalid', async function() {
 
-        validator.validateXML({ file: 'test/umlauts-invalid.xml' }, INCLUDE_SCHEMA, function(err, result) {
-          expect(err).to.exist;
-          done();
-        });
+        // given
+        let err;
+
+        // when
+        try {
+          await validator.validateXML({ file: 'test/umlauts-invalid.xml' }, INCLUDE_SCHEMA);
+        } catch (_err) {
+          err = _err;
+        }
+
+        // then
+        expect(err).to.exist;
       });
 
     });
@@ -266,26 +296,29 @@ describe('validator', function() {
 
     describe('with import', function() {
 
-      it('valid', function(done) {
+      it('valid', async function() {
 
-        validator.validateXML({ file: 'test/umlauts.xml' }, IMPORT_SCHEMA, function(err, result) {
+        // when
+        const { valid } = await validator.validateXML({ file: 'test/umlauts.xml' }, IMPORT_SCHEMA);
 
-          if (err) {
-            done(err);
-          } else {
-            expect(result.valid).to.be.true;
-            done();
-          }
-        });
+        // then
+        expect(valid).to.be.true;
       });
 
 
-      it('invalid', function(done) {
+      it('invalid', async function() {
 
-        validator.validateXML({ file: 'test/umlauts-invalid.xml' }, IMPORT_SCHEMA, function(err, result) {
-          expect(err).to.exist;
-          done();
-        });
+        // given
+        let err;
+
+        try {
+          await validator.validateXML({ file: 'test/umlauts-invalid.xml' }, IMPORT_SCHEMA);
+        } catch (_err) {
+          err = _err;
+        }
+
+        // then
+        expect(err).to.exist;
       });
 
     });
@@ -293,30 +326,31 @@ describe('validator', function() {
 
     describe('with xi:include', function() {
 
-      it('valid', function(done) {
+      it('valid', async function() {
 
-        validator.validateXML({ file: 'test/xi-include.xml' }, OTHER_SCHEMA, function(err, result) {
+        // when
+        const { valid } = await validator.validateXML({ file: 'test/xi-include.xml' }, OTHER_SCHEMA);
 
-          if (err) {
-            done(err);
-          } else {
-            expect(result.valid).to.be.true;
-            done();
-          }
-        });
-
+        // then
+        expect(valid).to.be.true;
       });
+
 
       // TODO(nikku): not supported during validation,
       // cf. https://github.com/nikku/node-xsd-schema-validator/issues/10
-      it.skip('invalid', function(done) {
+      it.skip('invalid', async function() {
 
-        validator.validateXML({ file: 'test/xi-include-invalid.xml' }, OTHER_SCHEMA, function(err, result) {
+        // given
+        let err;
 
-          expect(err).to.exist;
-          done();
-        });
+        try {
+          await validator.validateXML({ file: 'test/xi-include-invalid.xml' }, OTHER_SCHEMA);
+        } catch (_err) {
+          err = _err;
+        }
 
+        // then
+        expect(err).to.exist;
       });
 
     });
@@ -326,12 +360,19 @@ describe('validator', function() {
 
   describe('should not validate unsupported type for xml', function() {
 
-    it('valid', function(done) {
+    it('valid', async function() {
 
-      validator.validateXML([ 'this is not valid' ], BPMN_SCHEMA, function(err, result) {
-        expect(err).to.exist;
-        done();
-      });
+      // given
+      let err;
+
+      try {
+        await validator.validateXML([ 'this is not valid' ], BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
+
+      // then
+      expect(err).to.exist;
     });
 
   });
@@ -339,7 +380,7 @@ describe('validator', function() {
 
   describe('should handle missing Java', function() {
 
-    var JAVA_HOME;
+    let JAVA_HOME;
 
     beforeEach(function() {
       JAVA_HOME = process.env.JAVA_HOME;
@@ -349,30 +390,33 @@ describe('validator', function() {
       process.env.JAVA_HOME = JAVA_HOME;
     });
 
-    var validXML = (
+    const validXML = (
       '<?xml version="1.0" encoding="UTF-8"?>' +
       '<bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="simple" targetNamespace="http://activiti.org/bpmn">' +
       '</bpmn2:definitions>'
     );
 
 
-    it('Java JRE (validation)', function(done) {
+    it('Java JRE (validation)', async function() {
 
       // given
       process.env.JAVA_HOME = '/no-where';
 
+      let err;
+
       // when
-      validator.validateXML(validXML, BPMN_SCHEMA, function(err, result) {
+      try {
+        await validator.validateXML(validXML, BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
 
-        // then
-        expect(err).to.exist;
-
-        done();
-      });
+      // then
+      expect(err).to.exist;
     });
 
 
-    it('Java JDK (compilation)', function(done) {
+    it('Java JDK (compilation)', async function() {
 
       // given
       process.env.JAVA_HOME = '/no-where';
@@ -380,14 +424,17 @@ describe('validator', function() {
       // and no pre-compiled binary exists
       cleanup();
 
+      let err;
+
       // when
-      validator.validateXML(validXML, BPMN_SCHEMA, function(err, result) {
+      try {
+        await validator.validateXML(validXML, BPMN_SCHEMA);
+      } catch (_err) {
+        err = _err;
+      }
 
-        // then
-        expect(err).to.exist;
-
-        done();
-      });
+      // then
+      expect(err).to.exist;
     });
 
   });
