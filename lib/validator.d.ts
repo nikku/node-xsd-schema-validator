6b8059c8947007fd8768167d46b77658957eae26
Nico Rehwaldt <git_nikku@nixis.de>
2024-02-18 17:08:50 +0100

    feat: improve type definitions
    Closes https://github.com/nikku/node-xsd-schema-validator/issues/41

--- lib/validator.d.ts
+++ lib/validator.d.ts
@@ -1,8 +1,17 @@
-export function validateXML(
-    xml: string | ReadableStream | { file: string },
-    pathToXsd: string
-): Promise<{
+import { Readable as ReadableStream } from "stream";
+
+export type ValidateResult = {
     valid: boolean;
     result: string;
     messages: string[];
-}>;
+};
+
+export function validateXML(
+    xml: string | Buffer| ReadableStream | { file: string },
+    pathToXsd: string
+): Promise<ValidateResult>;
+
+/**
+ * @internal
+ */
+export function setup(): Promise<void>;
